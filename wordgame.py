1d7c21bb34b48012a56d90b672678b25414459e2
athulya <athulyajolly7@gmail.com>
2024-12-01 23:09:58 +0530

    Update wordgame.py

--- wordgame.py
+++ wordgame.py
@@ -1,24 +1,31 @@
-word=list(input('Enter a word:'))
-n=['_' for letter in word]
+word = list(input('Enter a word: '))
+n = ['_' for letter in word]
 print(n)
-a=len(word)
-for j in range(a+2):
-    guess=input('Guess a letter:')
+
+a = len(word)
+guessed_letters = []  # List to track guessed letters
+
+for j in range(a + 2):
+    guess = input('Guess a letter: ')
+    
+    if guess in guessed_letters:
+        print("You already guessed that letter. Try again.")
+        continue  # Skip the rest of the loop and ask for another guess
+
+    guessed_letters.append(guess)  # Add the guessed letter to the list
+
     if guess in list(word):
         for i in range(a):
-            if word[i]==guess:
-                n[i]=(guess)
-                print(n)
-                print('guess is correct')
-                break
+            if word[i] == guess:
+                n[i] = guess
+        print(n)
+        print('Guess is correct')
     else:
-        print('wrong letter guessed,try again')
-if n==word:
-    print('You won the game')
+        print('Wrong letter guessed, try again')
+
+    # Check if the word is completely guessed
+    if n == word:
+        print('You won the game!')
+        break
 else:
-    print('You lost')           
-                    
-        
-
-
-
+    print('You lost the game')
